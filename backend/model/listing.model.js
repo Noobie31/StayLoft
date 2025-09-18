05a437f94adfab70b92489c4b860088897d6c095
Noobie31 <yuvan.msky@gmail.com>
2025-09-18 19:53:27 +0530

    hssdad

--- backend/model/listing.model.js
+++ backend/model/listing.model.js
@@ -22,15 +22,13 @@ const listingSchema = new mongoose.Schema({
     },
     image1:{
         type:String,
-        required:true
     },
     image2:{
         type:String,
-        required:true
     },
     image3:{
         type:String,
-        required:true
+        
     },
     rent:{
         type:Number,
